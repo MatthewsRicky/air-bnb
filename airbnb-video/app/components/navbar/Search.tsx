bb8f0820210f49083e6dfc26c613bdec732c714e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-05-30 16:20:18 +0300

    Update Search.tsx

--- airbnb-video/app/components/navbar/Search.tsx
+++ airbnb-video/app/components/navbar/Search.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 const Search = () => {
 
   return (
-    <input type="text" value="Search" />
+    <div className=" border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+      <div className="flex flex-ro items-center justify-between">
+        Anywhere
+      </div>
+    </div>
+
   )
 }
 
