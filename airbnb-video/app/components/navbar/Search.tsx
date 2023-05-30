cedd6a0b507beb97cabfa96b2530e6e17e1049ac
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-05-30 17:09:05 +0300

    Update Search.tsx

--- airbnb-video/app/components/navbar/Search.tsx
+++ airbnb-video/app/components/navbar/Search.tsx
@@ -5,7 +5,12 @@ const Search = () => {
   return (
     <div className=" border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
       <div className="flex flex-ro items-center justify-between">
-        Anywhere
+        <div className="text-sm font-semibold px-6">
+          Anywhere
+        </div>
+        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+          Any Week
+        </div>
       </div>
     </div>
 
