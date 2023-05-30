ec2986689d75b7634e14fcf1cf82c589ae5cfb50
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-05-30 14:51:21 +0300

    Update Container.tsx

--- airbnb-video/app/components/Container.tsx
+++ airbnb-video/app/components/Container.tsx
@@ -8,7 +8,7 @@ const Container: React.FC<ContainerProps> = ({
   children
 }) => {
   return (
-    <div  className="max-w-[2520px] xl:px-2 md:px-10  sm:px-2">
+    <div  className="max-w-[2520px] xl:px-2 md:px-10 sm:px-2 px-4">
       {children}
     </div>
   )
