8658402f04c10a3585fbabbfb22286e9e6193648
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-05-30 20:27:41 +0300

    Update MenuItem.tsx

--- airbnb-video/app/components/navbar/MenuItem.tsx
+++ airbnb-video/app/components/navbar/MenuItem.tsx
@@ -1,8 +1,21 @@
 "Use client";
 
-const MenuItem = () => {
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({
+  onClick,
+  label
+}) => {
   return (
-    MenuItem
+    <div
+    onClick={onClick} 
+    className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+      {label}
+    </div>
+    
   )
 }
 
