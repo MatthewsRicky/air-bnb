98046e5c0a8d98e9b9d1900595b9bf8f69b77069
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-05-30 14:39:33 +0300

    Update Navbar.tsx

--- airbnb-video/app/components/navbar/Navbar.tsx
+++ airbnb-video/app/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
-          
+          Anything
         </Container>
       </div>
     </div>
