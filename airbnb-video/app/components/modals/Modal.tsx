7b9368b50796d539a55757041248a0fa4dd7f768
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-05-31 10:01:51 +0300

    Update Modal.tsx

--- airbnb-video/app/components/modals/Modal.tsx
+++ airbnb-video/app/components/modals/Modal.tsx
@@ -1,6 +1,8 @@
 const Modals = () => {
   return(
-    <div></div>
+    <div>
+      <h1 className="text-rose-500 text-xl transition hover:text-emerald-500 hover:scale-105  cursor-pointer">What is modals?</h1>
+    </div>
   )
 }
 
