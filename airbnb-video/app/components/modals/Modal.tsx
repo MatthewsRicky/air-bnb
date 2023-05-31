49a001245749856c67a59e06ef8652689e68e270
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-05-31 21:47:12 +0300

    Update Modal.tsx

--- airbnb-video/app/components/modals/Modal.tsx
+++ airbnb-video/app/components/modals/Modal.tsx
@@ -144,7 +144,11 @@ const Modal: React.FC<ModalProps> = ({
             {/*FOOTER*/}
             <div className="flex flex-col gap-2 p-6">
               <div className="flex flex-row items-center gap-4 w-full">
-                <Button label="My Button" /> 
+                <Button 
+                  disabled={disabled}
+                  label={actionLabel}
+                  onClick={handleSubmit}
+                /> 
               </div>
             </div>
           </div>
