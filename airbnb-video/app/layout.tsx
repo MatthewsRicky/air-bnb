a9f0e994cb974b7053573529063ba51b29899a6f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-01 12:38:07 +0300

    Update layout.tsx

--- airbnb-video/app/layout.tsx
+++ airbnb-video/app/layout.tsx
@@ -3,7 +3,8 @@ import { Nunito } from "next/font/google";
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata = {
   title: 'Airbnb',
@@ -23,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal actionLabel="Submit" secondaryActionLabel="Cancel" title="Hello World" isOpen/>
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         
