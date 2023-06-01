30e3d75b775dbcb8eff0aacabdfef3e336fbb74a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-01 12:51:27 +0300

    Update RegisterModal.tsx

--- airbnb-video/app/components/modals/RegisterModal.tsx
+++ airbnb-video/app/components/modals/RegisterModal.tsx
@@ -1,6 +1,34 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useRegisterModal from "../../hooks/useRegisterModal";
+
 const RegisterModal = () => {
+
+  const RegisterModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors
+    } 
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  });
+
   return(
-    <div>Reg</div>
+    <div></div>
   )
 }
 
