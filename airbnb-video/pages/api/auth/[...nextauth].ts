3ef7762b6cd9ba0df1d5750a3dce322c5a4acaf8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-02 14:22:23 +0300

    Update [...nextauth].ts

--- airbnb-video/pages/api/auth/[...nextauth].ts
+++ airbnb-video/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,9 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/google"
+import bcrypt from "bcrypt";
+
 
 import prisma from "@/app/libs/prismadb";
 
@@ -16,5 +19,37 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_CLIEN_SECRET as string,
     }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text'},
+        password: { label: 'password', type: 'password'},
+      },
+    async authorize(credentials) {
+      if (!credentials?.email || !credentials?.password) {
+        throw new Error('invalid credentials');
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email: credentials.email
+        }
+      });
+
+      if (!user || !user?.hashedPassword) {
+        throw new Error('Invalid credentials')
+      }
+      const isCorrectPassword = bcrypt.compare(
+        credentials.password,
+        user.hashedPassword
+      );
+
+      if (!isCorrectPassword) {
+        throw new Error('invalid  credentials');
+      }
+
+      return user;
+    }
+    })  
   ]
 }
