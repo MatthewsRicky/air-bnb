b417929245581fa997dc01326d7d602e1d35dc44
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-02 17:50:33 +0300

    Update route.ts

--- airbnb-video/app/api/route.ts
+++ airbnb-video/app/api/route.ts
@@ -1,14 +1,22 @@
-import bcrypt from "bcrypt";
+import prisma from "@/app/libs/prismadb";
+import bcrypt from "bcryptjs";
+import { Request } from "next";
 
-export async function POST(
-  request: Request
-) {
+export async function POST(request: Request) {
   const body = await request.json();
-  const {
-    email,
-    name,
-    password
-  } = body;
+  const { email, name, password } = body;
 
   const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
+
+  return new Response(JSON.stringify(user), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
