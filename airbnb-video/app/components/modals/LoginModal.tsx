df7645554bef2ff911362f1b2f723145ca7f965c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-03 12:33:42 +0300

    Update LoginModal.tsx

--- airbnb-video/app/components/modals/LoginModal.tsx
+++ airbnb-video/app/components/modals/LoginModal.tsx
@@ -100,14 +100,6 @@ const LoginModal = () => {
         icon={AiFillGithub}
         onClick={() => {}}
       />
-      <div className="text-neutral-500 text-center mt-4 font-light">
-        <div className="justify-center flex flex-row items-center gap-2">
-          <div>Already have an account?</div>
-          <div
-          onClick={registerModal.onClose} 
-          className="text-neutral-800 cursor-pointer hover:underline">Log in</div>
-        </div>
-      </div>
     </div>
     
   )
