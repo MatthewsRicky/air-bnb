dc0d18ff12fd9d2995d0ae350fa9cc48d9d5e5b0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-04 19:15:24 +0300

    Update route.ts

--- airbnb-video/app/api/register/route.ts
+++ airbnb-video/app/api/register/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client";
-//import prisma from "@/app/libs/prismadb";
+import prisma from "../../libs/prismadb";
 
-const prisma = new PrismaClient();
 
 export async function POST(
   request: Request, 
